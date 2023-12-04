0c56ba719f92e59e462858f3b109d7a32b0d5f18
athi-1012 <111171543+athi-1012@users.noreply.github.com>
2023-12-04 11:06:18 +0530

    Add files via upload

---
+++ portfolio1/script.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+function submitForm() {
+    alert('Form submitted!'); 
+    document.getElementById('contactForm').reset(); 
+}
